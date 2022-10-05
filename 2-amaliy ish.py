78e1c379fd136f11b45d92acacbb76b653af7362
axax0902a <113444011+axax0902a@users.noreply.github.com>
2022-10-05 10:05:21 +0300

    Add files via upload

---
+++ 2-amaliy ish.py
@@ -0,0 +1,7 @@
+import math
+s=float(input("doiraning yuzini kiriting s="))
+PI=3.14
+d=math.sqrt((4*s)/PI)
+r=d/2
+l=2*PI*r
+print(f"aylananing diametri {d} ga teng \n aylananing uzunligi {l} ga teng")
